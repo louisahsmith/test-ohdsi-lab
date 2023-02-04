3bee765462cf23cd95d60b57b38fd16d41a4c7ef
Louisa Smith (OHDSI lab) <louisahsmith@gmail.com>
2023-02-04 12:11:52 -0500

    use DatabaseConnector to connect instead

--- test_connection.R
+++ test_connection.R
@@ -1,12 +1,17 @@
 library(dplyr)
 library(dbplyr)
-library(DBI)
-con <- DBI::dbConnect(odbc::odbc(), "pharmetrics", timeout = Inf)
-# set the schema to avoid having to reference it
-DBI::dbGetQuery(con, "set search_path to omop_cdm_53_pmtx_202203")
-person <- tbl(con, "person") 
-# or person <- tbl(con, in_schema("omop_cdm_53_pmtx_202203", "person"))
+library(DatabaseConnector)
+
+con <- connect(dbms = "postgresql",
+               server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
+               port = 5439,
+               user = "usr9",
+               password = "2H2pS9G6ShKq")
+
+person <- tbl(con, inDatabaseSchema("omop_cdm_53_pmtx_202203", "person"))
 
 tally(person) # 34808145
 demographics <- count(person, gender_concept_id, ethnicity_concept_id, race_concept_id)
 demographics # only gender, matches what atlas says
+
+disconnect(con)
